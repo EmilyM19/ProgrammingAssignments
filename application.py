0b0d2895b6e66308d74e8db00bf40175d7bb9597
EmilyM19 <117695476+EmilyM19@users.noreply.github.com>
2023-11-19 17:47:27 -0500

    Update application.py

--- application.py
+++ application.py
@@ -28,3 +28,27 @@ def get_books():
         book_data = {'book name':book.book_name, 'author':book.author}
         output.append(book_data)
     return {'books': output}
+
+@app.route('/books/<id>')
+def get_book(id):
+    book = Book.query.get_or_404(id)
+    return {'book name':book.book_name, 'author': book.author, 
+            'publisher':book.publisher}
+    
+@app.route('/books', methods = ['POST'])
+def add_book():
+    book = Book(book_name = request.json['author_name'], 
+                author = request.json['author'],
+                publisher = request.json['publisher']) 
+    db.session.add(book)
+    db.session.commit()
+    return {'id': book.id}
+
+@app.route('/books/<id>', methods = ['DELETE'])
+def delete_book(id):
+    book = Book.query.get(id)
+    if book is None:
+        return {"error": "not found"}
+    db.session.delete(book)
+    db.session.commit()
+    return {"message": "deleted!"}
