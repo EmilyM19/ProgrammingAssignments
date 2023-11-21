79a4e428b23a997d8de870b8bf3e68ff95d1740d
EmilyM19 <117695476+EmilyM19@users.noreply.github.com>
2023-11-21 10:36:16 -0500

    M03CaseStudy_Mullins.py

--- M03CaseStudy_Mullins.py
+++ M03CaseStudy_Mullins.py
@@ -1,7 +1,18 @@
+"""
+Author:  Emily Mullins
+Date written: 11/19/2023
+File: M03CaseStudy_Mullins.py
+Short Desc: This program will create a class for Vehicle and a subclass of Vehicle Automobile.
+A user will be able to enter in information about a vehicle and receive output of the information.
+            
+"""
+
+#Creating class Vehicle 
 class Vehicle():
     def __init__(self, vehicle_type) -> None:
         self.vehicle_type = vehicle_type
                  
+#Creating subclass Automobile
 class Automobile(Vehicle):
     def __init__(self, vehicle_type, year, make, model, doors, roof) -> None:
         super().__init__(vehicle_type)
@@ -11,6 +22,7 @@ class Automobile(Vehicle):
         self.doors = doors
         self.roof = roof
 
+#Function to get door numbers, 2 or 4, and return. Must be int, must be 2 or 4.
 def get_doors():
     try:
         doors = int(input("Enter the number of doors (2 or 4): "))
@@ -18,12 +30,12 @@ def get_doors():
         print("Invalid input. Please enter a valid number of doors.")
         return get_doors()
     if doors not in [2, 4]:
-       print("Please enter a valid number of doors.") 
+       print("Please enter a valid number of doors (2 or 4).") 
        return get_doors()
     else:
         return doors
         
-
+# Function to get year input and return. Must be int.
 def get_year():
     try:
        year = int(input('Enter the vehicle year: ')) 
@@ -33,7 +45,7 @@ def get_year():
         return get_year()
         
     
-
+# function to get roof type (solid or sunroof) and return
 def get_roof():
     roof = input('Enter the vehicle roof type (Solid or Sunroof): ').strip().lower()
     if roof not in ['solid', 'sunroof']:
@@ -42,7 +54,7 @@ def get_roof():
     else:
         return roof
 
-
+# Get input, store values
 vehicle_type = input('Enter the vehicle type: ')
 year = get_year()
 make = input('Enter the vehicle make: ')
@@ -50,7 +62,10 @@ model = input('Enter the vehicle model: ')
 doors = get_doors()
 roof = get_roof()
 
+#create Automobile object.
 car1 = Automobile(vehicle_type, year, make, model, doors, roof)
+
+#Print automobile object info
 print()
 print(f'Vehicle Type: {car1.vehicle_type} \n'
 f'Vehicle Year: {car1.year} \n'
